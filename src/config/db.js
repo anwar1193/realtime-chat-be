0b77175dbec2fcf1dc14c46cc0286378699a88fa
Munawar Ahmad <anwarahmad391@gmail.com>
2022-11-26 13:19:05 +0700

    set deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,11 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-  // user: process.env.PGUSER,
-  // host: process.env.PGHOST,
-  // database: process.env.PGDATABASE,
-  // password: process.env.PGPASSWORD,
-  // port: process.env.PGPORT,
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USERNAME,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_DATABASE,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+  URI: process.env.DB_URI,
 });
 
 module.exports = pool;
